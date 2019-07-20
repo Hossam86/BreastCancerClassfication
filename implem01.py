4a2a64ce8a9614d2fa9ac762f85a7cddaa20c7ca
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 21:49:43 +0200

    testing

--- implem01.py
+++ implem01.py
@@ -1,6 +1,6 @@
 import numpy as nb
 from sklearn.datasets import load_breast_cancer
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -19,3 +19,27 @@ random_forest = RandomForestClassifier(n_estimators=100)
 ## Train the classifiers
 decision_tree.fit(X_train, y_train)
 random_forest.fit(X_train, y_train)
+
+# Create Predictions
+dt_pred = decision_tree.predict(X_test)
+rf_pred = random_forest.predict(X_test)
+
+# Check the performance of each model
+print('Decision Tree Model')
+print(classification_report(y_test, dt_pred, target_names=bc.target_names))
+
+print('Random Forest Model')
+print(classification_report(y_test, rf_pred, target_names=bc.target_names))
+
+#Graph our confusion matrix
+dt_cm = confusion_matrix(y_test, dt_pred)
+rf_cm = confusion_matrix(y_test, rf_pred)
+
+# Get our features and weights
+feature_list = sorted(zip(map(lambda x: round(x, 2), random_forest.feature_importances_), bc.target_names),
+             reverse=True)
+
+# Print them out
+print('feature\t\timportance')
+print("\n".join(['{}\t\t{}'.format(f,i) for i,f in feature_list]))
+print('total_importance\t\t',  sum([i for i,f in feature_list]))
