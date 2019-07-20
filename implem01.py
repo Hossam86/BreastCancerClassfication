e7711528e1c58d82a8c6d900f63627f383d6aed3
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 21:31:41 +0200

    split data

--- implem01.py
+++ implem01.py
@@ -1,8 +1,12 @@
+import numpy as nb
 from sklearn.datasets import load_breast_cancer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,classification_report
+from sklearn.tree import DecisionTreeClassifier
 
 bc=load_breast_cancer()
 X=bc.data
 y=bc.target
+
+# creat our test/train split
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42)
