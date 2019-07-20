782076aa4d14ddb8a6ddd85c29bd296e1a6dc438
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 23:52:25 +0200

    loading the data

--- implem02.py
+++ implem02.py
@@ -12,3 +12,41 @@ from sklearn.model_selection import GridSearchCV, train_test_split
 plt.style.use("ggplot")
 pd.set_option("display.max_columns", 500)
 
+# loading the data
+
+# loading the data
+names = [
+    "id_number",
+    "diagnosis",
+    "radius_mean",
+    "texture_mean",
+    "perimeter_mean",
+    "area_mean",
+    "smoothness_mean",
+    "compactness_mean",
+    "concavity_mean",
+    "concave_points_mean",
+    "symmetry_mean",
+    "fractal_dimension_mean",
+    "radius_se",
+    "texture_se",
+    "perimeter_se",
+    "area_se",
+    "smoothness_se",
+    "compactness_se",
+    "concavity_se",
+    "concave_points_se",
+    "symmetry_se",
+    "fractal_dimension_se",
+    "radius_worst",
+    "texture_worst",
+    "perimeter_worst",
+    "area_worst",
+    "smoothness_worst",
+    "compactness_worst",
+    "concavity_worst",
+    "concave_points_worst",
+    "symmetry_worst",
+    "fractal_dimension_worst",
+]
+breast_cancer = pd.read_csv("DataSets/breast_cancer/wdbc.data",names=names)
