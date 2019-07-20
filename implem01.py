e97989cd61685ba733fd8e503233dfd0a39655fa
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 21:32:44 +0200

    build and train

--- implem01.py
+++ implem01.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 bc=load_breast_cancer()
 X=bc.data
@@ -10,3 +11,11 @@ y=bc.target
 
 # creat our test/train split
 X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42)
+
+## build our models
+decision_tree = DecisionTreeClassifier()
+random_forest = RandomForestClassifier(n_estimators=100)
+
+## Train the classifiers
+decision_tree.fit(X_train, y_train)
+random_forest.fit(X_train, y_train)
