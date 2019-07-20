5c8046cc0a6165246208d571b5c6db806ac41160
Hossam Samir <Hossamsamir@outlook.com>
2019-07-20 21:24:24 +0200

    load datasets

--- implem01.py
+++ implem01.py
@@ -3,3 +3,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,classification_report
 
+bc=load_breast_cancer()
+X=bc.data
+y=bc.target
