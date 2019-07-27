31aed4c99bcd490af825d5a28ee2cc54ea669f8b
Hossam Samir <Hossamsamir@outlook.com>
2019-07-27 20:44:15 +0200

    print importance

--- utli.py
+++ utli.py
@@ -1,8 +1,36 @@
-def print_dx_perc(dataframe, col,dx):
+def print_dx_perc(dataframe, col, dx):
     """Function used to print class distribution for our data set"""
     dx_vals = dataframe[col].value_counts()
     dx_vals = dx_vals.reset_index()
     f = lambda x, y: 100 * (x / sum(y))
-    for i in range(0,len(dx)):
-        print('{0} accounts for {1:.2f}% of the diagnosis class'.format(dx[i],f(dx_vals[col].iloc[i],dx_vals[col])))
+    for i in range(0, len(dx)):
+        print(
+            "{0} accounts for {1:.2f}% of the diagnosis class".format(
+                dx[i], f(dx_vals[col].iloc[i], dx_vals[col])
+            )
+        )
 
+
+def variable_importance(importance, indices,names):
+    """
+    Purpose:
+    ----------
+    Prints dependent variable names ordered from largest to smallest
+    based on gini or information gain for CART model.
+
+    Parameters:
+    ----------
+    names:      Name of columns included in model
+    importance: Array returned from feature_importances_ for CART
+                   models organized by dataframe index
+    indices:    Organized index of dataframe from largest to smallest
+                   based on feature_importances_
+
+    Returns:
+    ----------
+    Print statement outputting variable importance in descending order
+    """
+    print("Feature Ranking : ")
+    for f in range (len(names)):
+        i=f
+        print("%d. the feature '%s' has a Mean Decrease in Gini of %f" % (f + 1,names[indices[i]],importance[indices[f]]))
